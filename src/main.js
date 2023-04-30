cdcacea64173f5673eb6eeea9a05da7bc4a9ec0f
Tomo-kw <t.kuwahara@miraito-inc.co.jp>
2023-04-30 19:31:59 +0900

    初期配列作成（2次元配列作成・石のセット）

--- src/main.js
+++ src/main.js
@@ -10,12 +10,13 @@ let player = [WHITE, BLACK];
 
 // ゲームスタート
 window.onload = () => {
-    console.log(player);
+    game();
 }
 
 // 配列を作成しレンダリングする
 const game = () => {
-
+    dataArray = createDataArray();
+    // console.log(dataArray);
 }
 
 // プレイヤーを返す関数
@@ -23,19 +24,32 @@ const whichPlayer = () => {
 
 }
 
-// 実際に配列を作成し石の初期値をセットし返す関数
+// 2次元配列を作成し石の初期値をセットし返す関数
 const createDataArray = () => {
+    const baseArray = createBaseArray();
 
+    return initializeDataArray(baseArray);
 }
 
 // 2次元配列を作成
 const createBaseArray = () => {
-
+    const x = [...Array(8)].map(() => {
+        return undefined;
+    })
+    return x.map(() => {
+        return [...Array(8)].map(() => {
+            return undefined;
+        })
+    })
 }
 
 // createBaseArrayで作成した配列に石をセットする
-const initializeDataArray = () => {
-
+const initializeDataArray = (baseArray) => {
+    baseArray[3][3] = WHITE;
+    baseArray[4][4] = WHITE;
+    baseArray[3][4] = BLACK;
+    baseArray[4][3] = BLACK;
+    return baseArray;
 }
 
 // 石クリック時の処理セット関数
