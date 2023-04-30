75b4a6971b5570c8a4e27a3c21e625f6aad56236
Tomo-kw <t.kuwahara@miraito-inc.co.jp>
2023-04-30 19:16:22 +0900

    first commit（必要関数の記述）

---
+++ src/main.js
@@ -0,0 +1,72 @@
+// 設計
+// ゲームで使用する変数設定
+
+const WHITE = -1;
+const BLACK = 1;
+let dataArray;
+let turn = 1;
+let player = [WHITE, BLACK];
+
+
+// ゲームスタート
+window.onload = () => {
+    console.log(player);
+}
+
+// 配列を作成しレンダリングする
+const game = () => {
+
+}
+
+// プレイヤーを返す関数
+const whichPlayer = () => {
+
+}
+
+// 実際に配列を作成し石の初期値をセットし返す関数
+const createDataArray = () => {
+
+}
+
+// 2次元配列を作成
+const createBaseArray = () => {
+
+}
+
+// createBaseArrayで作成した配列に石をセットする
+const initializeDataArray = () => {
+
+}
+
+// 石クリック時の処理セット関数
+const handleCellClick = () => {
+
+}
+
+// セルを押下した際のロジック
+const check = () => {
+
+}
+
+// セルのindexチェック
+const validIndex = () => {
+
+}
+// 全体の？indexチェック
+const allValidIndex = () => {
+
+}
+// 石を返す関数
+const lineReverse = () => {
+
+}
+
+// 盤面を作り直す
+const lenderData = () => {
+
+}
+
+// 盤面を削除する関数
+const allRemove = () => {
+
+}
